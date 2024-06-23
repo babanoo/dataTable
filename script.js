79f397fec6c4b4d8c393f63d81eedeedb527f32f
babanoo <salwababano31@gmail.com>
2024-06-23 19:42:09 +0100

    summary

---
+++ script.js
@@ -0,0 +1,89 @@
+const table = document.querySelector(".table-container");
+
+let users = [
+  {
+    name: ["Justin Septimus", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$200", "USD"],
+  },
+  {
+    name: ["Anika Rahiel Madsen", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Overdue", "Dued on 15/APR/2020"],
+    amount: ["$300", "USD"],
+  },
+  {
+    name: ["Miracle Vaccaro", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$250", "USD"],
+  },
+  {
+    name: ["Erin Levin", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Unpaid", "Dues on 15/APR/2020"],
+    amount: ["$200", "USD"],
+  },
+  {
+    name: ["Mira Herwitz", "example@email.com"],
+    "user status": ["Inactive", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$200", "USD"],
+  },
+  {
+    name: ["Jaxson Siphron", "example@email.com"],
+    "user status": ["Inactive", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$750", "USD"],
+  },
+  {
+    name: ["Mira Levin", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Unpaid", "Dues on 15/APR/2020"],
+    amount: ["$200", "USD"],
+  },
+  {
+    name: ["LincoIn Levin", "example@email.com"],
+    "user status": ["Active", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$370", "USD"],
+  },
+  {
+    name: ["Phillip Saris", "example@email.com"],
+    "user status": ["Inactive", "Last Login: 14/APR/2020"],
+    "payment status": ["Unpaid", "Dues on 15/APR/2020"],
+    amount: ["$200", "USD"],
+  },
+  {
+    name: ["Cheyenne Ekstrom Bothman", "example@email.com"],
+    "user status": ["Inactive", "Last Login: 14/APR/2020"],
+    "payment status": ["Paid", "Paid on 15/APR/2020"],
+    amount: ["$150", "USD"],
+  },
+];
+renderTable(users);
+function renderTable(users) {
+  const headers = Object.keys(users[0]);
+  const headerRow = document.createElement("tr");
+
+  for (let header of headers) {
+    const tableHeader = document.createElement("th");
+    headerRow.appendChild(tableHeader);
+    tableHeader.textContent = header.toUpperCase();
+  }
+  table.appendChild(headerRow);
+
+  users.forEach((user) => {
+    const objValues = Object.values(user);
+    const contentRow = document.createElement("tr");
+    objValues.forEach((el) => {
+      const tableContent = document.createElement("td");
+      contentRow.appendChild(tableContent);
+      tableContent.textContent = el;
+      typeof el === "object" ? (tableContent.innerHTML = el.join("<br>")) : el;
+    });
+
+    table.appendChild(contentRow);
+  });
+}
